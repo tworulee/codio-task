b314119a99cc427440958bc7b63bc272fa9067a5
Tuğrul Yüce <tugrulyuce80@gmail.com>
2024-07-16 13:13:56 +0300

    product-listing

---
+++ src/app/page.jsx
@@ -0,0 +1,22 @@
+import Products from "@/components/Products";
+import React from "react";
+
+const Page = async () => {
+  const res = await fetch(
+    "https://66957d684bd61d8314cb71a8.mockapi.io/codio/product",
+    { next: { revalidate: 10000 } }
+  );
+
+  const data = await res.json();
+  console.log(data, "data");
+
+  return (
+    <div className='flex flex-wrap items-center justify-center  gap-8 '>
+      {data?.map((dt) => (
+        <Products  dt={dt} key={dt.id} />
+      ))}
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/components/Products.jsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Products = ({dt}) => {
+  return (
+    <div className='  border-2 rounded-md shadow-lg mb-5 ' >
+      <div className=' min-w-[390px] ml-3 mt-2'>
+      <Image className=' rounded'  width={380} height={400} src={dt.avatar} />
+      </div>
+      <div className='text-center m-4 '>
+      <h1 className='font-black'> {dt.name}</h1>
+      <h1>Marka: {dt.brand}</h1>
+      <h1>Fiyat: {dt.price}</h1>
+      <h1>Oluşturulma Zamanı: {dt.createdAt}</h1>
+      </div>
+      <div className='flex items-center justify-center gap-8'>
+        <button className='border-2 rounded-md p-2 hover:bg-slate-500 mb-3'>Düzenle</button>
+        <button className='border-2 rounded-md p-2 hover:bg-amber-300 mb-3'>Sil</button>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Products
